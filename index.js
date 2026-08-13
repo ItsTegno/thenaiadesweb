a1e930f66f3b813687ad1c3633802928ddcf9605
ItsTegno <davidmunozsegura8@gmail.com>
2026-08-13 23:08:15 +0200

    Added floating header, some minor improvements on the visual part and for some reason, CARACOL?

--- index.js
+++ index.js
@@ -1,1 +1,86 @@
-//No le hagais caso a todo lo que veais que esto está muy en pañales y hay que arreglar cosillas
+////////////////////
+//     Easing     //
+////////////////////
+
+function lerp(a, b, t)
+{
+    return a + (b - a) * t;
+}
+
+function ease(t)
+{
+    return (t * t) * (3 - 2 * t);
+}
+
+function easeInOut(t) {
+    return t < 0.5
+        ? 2 * t * t
+        : 1 - Math.pow(-2 * t + 2, 2) / 2;
+}
+
+function easeOutExpo(t) {
+    return t === 1 ? 1 : 1 - Math.pow(2, -10 * t);
+}
+
+function easeInExpo(t) {
+    return t === 0 ? 0 : Math.pow(2, 10 * (t - 1));
+}
+
+const panelColor = getComputedStyle(document.documentElement)
+    .getPropertyValue('--panel')
+    .trim();
+const lineColor = getComputedStyle(document.documentElement)
+    .getPropertyValue('--line')
+    .trim();
+
+
+////////////////////
+//     Scroll     //
+////////////////////
+
+const parallaxFactor = -0.5
+
+window.addEventListener('scroll', function ()
+{
+    window.requestAnimationFrame(function ()
+    {
+        const scrollY = window.scrollY;
+        const maxScroll = window.innerHeight;
+
+        ////////////////////////
+        //     Background     //
+        ////////////////////////
+
+        const backgroundProgress = Math.min(scrollY / maxScroll, 1);
+        const bg = document.getElementById('background');
+        if (bg)
+        {
+            //Blur
+            const backgroundBlurAmount = backgroundProgress * 35;
+            bg.style.filter = `blur(${backgroundBlurAmount}px)`;
+
+            //Parallax
+            const parallax = scrollY * parallaxFactor; // Ajusta el factor
+            //bg.style.transform = `translateY(${parallax}px)`;
+        }
+
+        ////////////////////
+        //     HEADER     //
+        ////////////////////
+
+        const header = document.getElementsByTagName('header')[0];
+        const headerProgress = Math.min(scrollY / maxScroll * 4, 1);
+
+        if (header)
+        {
+            //Blur
+            const headerBlurAmount = headerProgress * 10;
+
+            header.style.backdropFilter = `blur(${headerBlurAmount}px)`;
+            header.style.background =
+                `color-mix(in srgb, transparent ${100 - headerProgress * 100}%, ${panelColor})`;
+            header.style.borderBottom =
+                `0.5px solid color-mix(in srgb, transparent ${100 - headerProgress * 100}%, ${lineColor})`;
+        }
+    });
+});
